438de4d89d1104b2a58f6407c8cef6d682bf0b51
g-nitin <83675678+g-nitin@users.noreply.github.com>
2025-05-04 17:31:51 -0400

    15th outputs

--- job.sh
+++ job.sh
@@ -14,5 +14,5 @@ module load python3/anaconda/2021.07 gcc/12.2.0 cuda/12.1
 echo $CONDA_DEFAULT_ENV
 
 hostname
-uv run python3 code/procedural_text_process.py data/allrecipes-recipes outs/outs_14 --limit 200 --log-level TRACE
-zip -r outs/outs_13.zip outs/outs_13 &> zipp.out
+uv run python3 code/procedural_text_process.py data/allrecipes-recipes outs/outs_15 --limit 200 --log-level TRACE
+zip -r outs/outs_15.zip outs/outs_15 &> zip.out
